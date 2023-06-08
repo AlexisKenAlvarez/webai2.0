cc2b029c540428d8a7754e6e05bf4e172767b087
Alexis Ken Alvarez <alexisken1432@gmail.com>
2023-06-08 19:04:58 +0800

    rebrand

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,13 +10,18 @@ module.exports = {
         ginger: ['Ginger Bold'],
         roboto: ['Roboto Mono'],
         poppins: ['Poppins'],
-        asap: ['asap']
+        asap: ['asap'],
+        righteous: ['Righteous']
       },
       colors: {
         mywhite: '#F4F4F4',
         myblack: '#28292A',
         pongkan: '#f86449',
-        secondarybg: '#F3F7F6'
+        secondarybg: '#F3F7F6',
+        mycyan: '#2BCFED',
+        mypink: '#E813CA',
+        subtleblack: '#0f0e0e',
+        mypurple: '#9D5CDF'
       },
       dropShadow: {
         whiteShad: '0px 0px 8px #ffffff'
